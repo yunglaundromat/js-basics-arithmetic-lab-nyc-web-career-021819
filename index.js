66e2fdd6a0a58a9f045dca4d3329b04aa18d9ae3
Nathaniel Cain <nathaniel2897@gmail.com>
2019-03-31 16:23:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+let newID = 1000000000 + oldID
+
+ let ageIsValid=Number.isInteger(currentAge)
+
+ let randomNumber = Math.floor(Math.random() * 20)
+
+ let randomInteger = Math.floor(randomNumber)
+
+ let randomUserID = Math.floor(Math.random() * 20) + 1000000001
 /*
 Variables defined in the test suite:
 - oldID
